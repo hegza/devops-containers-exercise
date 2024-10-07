b725c4d942b29c51e39c688fe305ee5daf228b5b
Henri Lunnikivi <heggggxa@gmail.com>
2024-10-07 15:51:05 +0300

    feat(go): use getsysinfo to respond with required JSON

--- service-go/main.go
+++ service-go/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"internal/getsysinfo"
 	"net/http"
 )
 
@@ -22,6 +24,44 @@ func main() {
 	select {}
 }
 
+// Struct to hold the IP address in JSON format
+type SysInfo struct {
+	IPs            []string `json:"ips"`
+	Ps_ax          string   `json:"ps_ax"`
+	AvailableBytes uint64   `json:"available_bytes"`
+	UptimeSecs     uint64   `json:"uptime_secs"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go!")
+	ips, err := getsysinfo.GetIpAddress()
+	if err != nil {
+		http.Error(w, "Unable to get IP address", http.StatusInternalServerError)
+		return
+	}
+
+	ps_ax, err := getsysinfo.GetPsAx()
+	if err != nil {
+		http.Error(w, "Unable to get output of `ps -ax`", http.StatusInternalServerError)
+		return
+	}
+
+	available_bytes, err := getsysinfo.GetAvailableBytesOnRoot()
+	if err != nil {
+		http.Error(w, "Unable to get available bytes on root partition", http.StatusInternalServerError)
+		return
+	}
+
+	uptime_secs, err := getsysinfo.GetUptime()
+	if err != nil {
+		http.Error(w, "Unable to get uptime", http.StatusInternalServerError)
+		return
+	}
+
+	// Create a response object
+	response := SysInfo{IPs: ips, Ps_ax: ps_ax, AvailableBytes: available_bytes, UptimeSecs: uptime_secs}
+
+	// Convert the response to JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
 }
