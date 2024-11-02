3d1c02676ed0b0de66effbbb85efbf9abfd23794
Henri Lunnikivi <heggggxa@gmail.com>
2024-11-02 14:35:51 +0200

    feat(rs): print random identifier on startup

--- service-rs/src/main.rs
+++ service-rs/src/main.rs
@@ -9,6 +9,8 @@ use axum::{routing::get, Router};
 const LISTEN_ADDR: &str = "0.0.0.0:3000";
 const SERVICE_GO_URI: sync::LazyLock<&str> =
     sync::LazyLock::new(|| option_env!("GO_URI").unwrap_or("http://service-go:3000"));
+const ID: sync::LazyLock<&str> =
+    sync::LazyLock::new(|| random_word::gen_len(6, random_word::Lang::En).unwrap());
 
 #[tokio::main]
 async fn main() {
@@ -24,7 +26,7 @@ async fn main() {
 
     // Listen on TCP
     let listener = tokio::net::TcpListener::bind(LISTEN_ADDR).await.unwrap();
-    println!("Server listening for TCP at {LISTEN_ADDR}");
+    println!("Server '{}' listening for TCP at {LISTEN_ADDR}", *ID);
 
     // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
     axum::serve(listener, app).await.unwrap();
