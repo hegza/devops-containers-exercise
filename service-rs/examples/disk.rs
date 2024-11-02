494235699b216838aebfb9cae3577f09aea23ec7
Henri Lunnikivi <heggggxa@gmail.com>
2024-11-02 11:37:41 +0200

    test(disk): also print available bytes on rootfs

--- service-rs/examples/disk.rs
+++ service-rs/examples/disk.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use sysinfo::Disks;
 
 fn main() {
@@ -11,4 +13,20 @@ fn main() {
             disk.available_space()
         );
     }
+
+    println!("Bytes on rootfs: {}", available_bytes());
+}
+
+fn available_bytes() -> u64 {
+    let disks = Disks::new_with_refreshed_list();
+
+    let root_disk = disks
+        .into_iter()
+        .find(|d| d.mount_point() == PathBuf::from("/"))
+        // This will never happen as long as this application is run on a Linux machine (container
+        // or not)
+        .expect("rootfs is not mounted");
+
+    // Returns available space in bytes
+    root_disk.available_space()
 }
