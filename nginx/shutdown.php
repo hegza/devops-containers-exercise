febc96aac9a565f0c1e295ff685ec99cd614e496
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-11-05 21:03:20 +0200

    feat: improve output for /shutdown

--- nginx/shutdown.php
+++ nginx/shutdown.php
@@ -1,8 +1,14 @@
 <?php
 echo "Shutting down the server...<br>";
-$output = shell_exec('/bin/sh /var/www/scripts/shutdown.sh 2>&1');
-echo "output: $output<br>";
-echo "Done?<br>";
+$cmd = "/var/www/scripts/shutdown.sh";
+echo "Calling $cmd...<br>";
+$output = shell_exec('/var/www/scripts/shutdown.sh 2>&1');
+echo "Command returned: $output<br>";
+echo "<br><br>";
+echo "As one can see, this approach couldn't quite pass the Linux security measures.<br><br>";
+echo "I believe one appropriate approach would be to add shutdown functionality to all applications, <br>";
+echo "then request that functionality from the nginx server. However, I've got a course to run so <br>";
+echo "I gracefully give this one up. :)<br>";
 
 exit;
 ?>
